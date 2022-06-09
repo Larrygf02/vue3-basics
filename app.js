7e93dd49a4d218ccd5f93d330955688b126316d5
Larrygf02 <raulgf_02@hotmail.com>
2022-06-08 21:35:21 -0500

    conditional rendering

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const app = Vue.createApp({
         return {
             title: 'The final empire',
             author: 'Brandon Senderson',
-            age: 45
+            age: 45,
+            show: true
         }
     },
     methods: {
         changeTitle() {
             this.title = 'New final empire'
+        },
+        toggleBooks() {
+            this.show = !this.show
         }
     }
     // template: '<h2>I am the template now</h2>'
