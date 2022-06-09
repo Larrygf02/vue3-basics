9622c64b457804c00e5063e864866c6fd4040c6f
Larrygf02 <raulgf_02@hotmail.com>
2022-06-08 21:18:42 -0500

    buttons and click events

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const app = Vue.createApp({
             author: 'Brandon Senderson',
             age: 45
         }
+    },
+    methods: {
+        changeTitle() {
+            this.title = 'New final empire'
+        }
     }
     // template: '<h2>I am the template now</h2>'
 })
